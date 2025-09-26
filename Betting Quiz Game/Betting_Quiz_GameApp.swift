95c43e35364b26994096cb8a02dda67145272d05
keshew <artyom.korotckov2017@yandex.ru>
2025-09-26 08:31:17 +0500

    Initial Commit

---
+++ Betting Quiz Game/Betting_Quiz_GameApp.swift
@@ -0,0 +1,17 @@
+//
+//  Betting_Quiz_GameApp.swift
+//  Betting Quiz Game
+//
+//  Created by Артём Коротков on 26.09.2025.
+//
+
+import SwiftUI
+
+@main
+struct Betting_Quiz_GameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
